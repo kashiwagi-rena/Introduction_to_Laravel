9084726d13384d73ab3354b11a3110388c0b6f4e
kashiwagi-rena <azisai07@gmail.com>
2023-09-02 16:18:10 +0900

    p.42 コントローラ内の説明

---
+++ app/Http/Controllers/HelloController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+//名前空間を指定している
+
+use Illuminate\Http\Request;
+//use文。Illuminate\Httpパッケージ内に用意してある「Request」を使える状態にしている。
+
+class ˙HelloController extends Controller
+{
+    //
+}
