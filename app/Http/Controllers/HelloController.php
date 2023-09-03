0ff5aa5395a33295173d3b59ad76fa76cf006db1
kashiwagi-rena <azisai07@gmail.com>
2023-09-03 10:54:24 +0900

    p.44 ルートパラメータの利用

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -6,23 +6,27 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 //use文。Illuminate\Httpパッケージ内に用意してある「Request」を使える状態にしている。
 
-class ˙HelloController extends Controller
+class HelloController extends Controller
 {
-    public function index() {
+    public function index($id='noname', $pass='unknomn') {
 
         return <<<EOF
 
 <html>
 <head>
-<title>Hello/Index</title>
-<style>
-body {font-size:16pt; color:#999; }
-h1 { font-size:100pt; text-align:right; color:#eee; margi:-40px 0px -50px 0px; }
-</style>
+    <title>Hello/Index</title>
+    <style>
+        body {font-size:16pt; color:#999; }
+        h1 { font-size:100pt; text-align:right; color:#eee; margi:-40px 0px -50px 0px; }
+    </style>
 </head>
 <body>
         <h1>Index</h1>
         <p>これは、Helloコントローラのindexアクションです。</p>
+        <ul>
+            <li>ID: {$id}</li>
+            <li>PASS: {$pass}</li>
+        </ul>
 </body>
 </html>
 EOF;
